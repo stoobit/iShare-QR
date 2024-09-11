02bb6b6caf9481971c5ed06b9fd7b689341a080e
stoobit <till.bruegmann@icloud.com>
2024-09-12 01:42:16 +0200

    opt

--- Share/ShareViewController.swift
+++ Share/ShareViewController.swift
@@ -23,8 +23,27 @@ class ShareViewController: UIViewController {
                 extensionContext: extensionContext
             ))
             
-            hostingView.view.frame = view.frame
+            addChild(hostingView)
+            hostingView.view
+                .translatesAutoresizingMaskIntoConstraints = false
+            
             view.addSubview(hostingView.view)
+            hostingView.didMove(toParent: self)
+            
+            NSLayoutConstraint.activate([
+                hostingView.view.widthAnchor.constraint(
+                    equalTo: view.widthAnchor, multiplier: 1
+                ),
+                hostingView.view.heightAnchor.constraint(
+                    equalTo: view.heightAnchor, multiplier: 1
+                ),
+                hostingView.view.centerXAnchor.constraint(
+                    equalTo: view.centerXAnchor
+                ),
+                hostingView.view.centerYAnchor.constraint(
+                    equalTo: view.centerYAnchor
+                )
+            ])
         }
     }
 }
