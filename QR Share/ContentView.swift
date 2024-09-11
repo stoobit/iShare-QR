464d0255889f6dcc819f39964e5c405da20b5355
stoobit <till.bruegmann@icloud.com>
2024-09-11 16:50:19 +0200

    Initial Commit

---
+++ QR Share/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QR Share
+//
+//  Created by Till Brügmann on 11.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QR Share/QR_ShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_ShareApp.swift
+//  QR Share
+//
+//  Created by Till Brügmann on 11.09.24.
+//
+
+import SwiftUI
+
+@main
+struct QR_ShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
